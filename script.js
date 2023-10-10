f3c1165a31c8c759506ba17221e059238c9d0f90
Mayanktyagi05 <mayanktyagee@gmail.com>
2023-10-10 13:25:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+function saveToLocalStorage(event){
+    event.preventDefault();
+  
+    const name = event.target.username.value;
+    const email = event.target.emailId.value;
+    const phonenumber = event.target.phonenumber.value;
+  
+    const obj={
+      name,
+      email,
+      phonenumber
+    }
+
+    axios.post("https://crudcrud.com/api/936a96b95a384840ae3de78ec36e6738/appointmentdata", obj)
+    .then((response) => {
+      showUserOnScreen(response.data)
+      console.log(response);
+    }).catch((err) => {
+      console.log(err);
+    });
+    //localStorage.setItem(obj.email, JSON.stringify(obj))
+    //showUserOnScreen(obj)
+  }
+  window.addEventListener("DOMContentLoaded", ()=>{
+    axios.get("https://crudcrud.com/api/936a96b95a384840ae3de78ec36e6738/appointmentdata")
+    .then((response) => {
+      for(var i=0; i<response.data.length; i++)
+      {
+        showUserOnScreen(response.data[i])
+      }
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+    console.log(data)
+  })
+  
+  function showUserOnScreen(obj){
+    const parentElem = document.getElementById('listofitems');
+  
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `${obj.name} - ${obj.email} - ${obj.phonenumber}`;
+  
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => {
+      deleteUserData(obj.email);
+      parentElem.removeChild(listItem);
+    });
+  
+    listItem.appendChild(deleteButton);
+    parentElem.appendChild(listItem);
+  }
+  
+  function deleteUserData(email) {
+    localStorage.removeItem(email);
+  }
